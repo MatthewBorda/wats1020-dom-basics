035d8c9ff191ea8835e861f2ed272a2560017c62
matthewmborda <matthewmborda@gmail.com>
2016-01-14 23:06:43 -0500

    End of day Jan 14 work

--- fortune-cookie-generator.js
+++ fortune-cookie-generator.js
@@ -3,36 +3,10 @@
 // Fortune Cookie Generator
 //
 //////////////////////////////////////////
-
-var generateFortuneCookie = function() {
-    // This is where your code for the Fortune Cookie generator goes.
-    // You will use the fortunesList variable defined lower in this file
-    // to supply your fortune cookies with text.
-
-    // TODO: Grab the paragraph with the ID
-    // `fortune-cookie-text` to be able to insert text into that element.
-
-    // TODO: Update the Previous Fortunes list with the current `innerHTML`
-    // value of `#fortune-cookie-text`. Follow these steps:
-        // 1. Create a new `li` element with the `document.createElement()` method.
-        // 2. Set the `innerHTML` of that element equal to the `innerHTML` of
-        //    the `#fortune-cookie-text` element.
-        // 3. Select the `#previous-fortunes-container` container and use
-        //    `appendChild()` to append the new `li` element you created above.
-        // 4. You should see the previous fortune cookie saying show up in the list.
-
-    // TODO: Select a new (random) fortune cookie saying from the data stored in the
-    // `fortunesList` variable. (HINT: You will use `Math.floor()` and
-    // `Math.random()` to accomplish this.) Use this data to update the
-    // `innerText` of the `#fortune-cookie-text` element.
-
-}
-
-// The following data list is provided for you to use in your code.
 var fortunesList = [
     "People are naturally attracted to you.",
     "You learn from your mistakes... You will learn a lot today.",
-    "If you have something good in your life, don't let it go!",
+     "If you have something good in your life, don't let it go!",
     "What ever you're goal is in life, embrace it visualize it, and for it will be yours.",
     "Your shoes will make you happy today.",
     "You cannot love life until you live the life you love.",
@@ -79,6 +53,67 @@ var fortunesList = [
     "You will be called in to fulfill a position of high honor and responsibility.",
     "The man on the top of the mountain did not fall there.",
     "You will conquer obstacles to achieve success.",
-    "Joys are often the shadows, cast by sorrows.",
-    "Fortune favors the brave."
+    "Joys are often the shadows, cast by sorrows."
+    
 ]
+
+var generateFortuneCookie = function() {
+
+
+    // TODO: Grab the paragraph with the ID
+    // `fortune-cookie-text` to be able to insert text into that element.
+    // TODO: Update the Previous Fortunes list with the current `innerHTML`
+    // value of `#fortune-cookie-text`. Follow these steps:
+        // 1. Create a new `li` element with the `document.createElement()` method.
+        // 2. Set the `innerHTML` of that element equal to the `innerHTML` of
+        //    the `#fortune-cookie-text` element.
+        // 3. Select the `#previous-fortunes-container` container and use
+        //    `appendChild()` to append the new `li` element you created above.
+        // 4. You should see the previous fortune cookie saying show up in the list.
+
+    // TODO: Select a new (random) fortune cookie saying from the data stored in the
+    // `fortunesList` variable. (HINT: You will use `Math.floor()` and
+    // `Math.random()` to accomplish this.) Use this data to update the
+    // `innerText` of the `#fortune-cookie-text` element.
+  //random fortune limited by floor and lenght of forutne array
+  //random fortune limited by floor and lenght of forutne array
+var randomFortune = Math.floor(Math.random()*fortunesList.length); //random fortune limited by floor and lenght of forutne array
+
+  
+if ( fortunesList.indexOf(
+  (document.getElementById("previous-fortunes-container").lastChild.innerHTML)) ==randomFortune) 
+  {document.getElementById("fortune-cookie-text").innerText = "Fortune favors the brave.";
+  }  else { document.getElementById("fortune-cookie-text").innerText = (fortunesList[randomFortune]); } 
+  //[randomFortune] represents the index of the list
+	
+	var createNew = document.createElement("li");
+  if ( fortunesList.indexOf(
+  (document.getElementById("previous-fortunes-container").lastChild.innerHTML)) ==randomFortune) 
+  {createNew.innerText = "Fortune favors the brave.";
+  }  else { createNew.innerText = (fortunesList[randomFortune]) } 
+	
+//   createNew.innerText = (fortunesList[randomFortune]);
+  
+ 	document.getElementById("previous-fortunes-container").appendChild(createNew);
+
+
+  setTimeout(function() {
+    createNew.className = createNew.className + " show";
+  }, 10);
+
+  twitterString = document.getElementById("previous-fortunes-container").lastChild.innerHTML;
+   twitterStringFormat = "http://twitter.com/intent/tweet?text=My+2016+Fortune:"+twitterString.replace(/\s+/g, '+') ;
+    document.getElementById('twitter').href=twitterStringFormat
+
+}
+
+// Click counter
+  var clicks = 0;
+    function clickCounter() {
+        clicks += 1;
+        document.getElementById("clicks").innerText = clicks;
+    }
+
+
+
+  
